71e0f2d76e9f2ecdc71ca30b25b2bd4ef243fe0e
Joseph Heinz <joheb41@student.oaisd.org>
2025-01-06 14:06:07 +0000

    aaron meier educated me on Console.ReadLine()

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public static class Program
         Console.WriteLine("Dice Game\n");
         Console.WriteLine("In this game you and a computer Rival will play 10 rounds\nwhere you will each roll a 6 - sided dice, and the player\nwith the highest dice value will win the round. The player\nwho wins the most rounds wins the game. Good luck!\n");
         Console.Write("Press any key to start...");
-        string throwAwayVariableBecauseInputIsNeeded = Console.ReadLine();
+        Console.ReadLine();
         int round = 1;
         int playerScore = 0;
         int rivalScore = 0;
@@ -33,7 +33,7 @@ public static class Program
             Console.WriteLine($"Round {round}");
             Console.WriteLine($"Rival rolled a {rivalRoll}");
             Console.Write("Press any key to roll the dice...");
-            int emptyRead = Console.Read();
+            Console.Read();
 
             Console.WriteLine($"You rolled a {playerRoll}");
             if (winLoss == 1) Console.WriteLine("You won this round.");
@@ -42,7 +42,7 @@ public static class Program
 
             Console.WriteLine($"The score is now - You: {playerScore}. Rival: {rivalScore}.");
             Console.Write("Press any key to continue...");
-            string emptyRead2 = Console.ReadLine();
+            Console.ReadLine();
             Console.WriteLine();
 
             round++;
